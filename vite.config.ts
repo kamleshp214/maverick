3ee1156d4708909caaaaad8c22a6cee6d56dcaa6
kamleshp214 <142132159+kamleshp214@users.noreply.github.com>
2025-05-25 22:23:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
-      ? [await import("@replit/vite-plugin-cartographer").then((m) => m.cartographer())]
-      : [],
-  ],
+  plugins: [react()],
+  root: "client",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
-  },
-  root: path.resolve(__dirname, "client"),
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
+      "@": path.resolve(__dirname, "./client/src")
+    }
   },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-      },
-    },
-  },
+        secure: false,
+      }
+    }
+  }
 });
